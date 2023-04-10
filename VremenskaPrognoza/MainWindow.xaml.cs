db371e2b26ec95caf153b0c61763c92fbdcdd686
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-04-10 17:53:54 +0200

    Add hours to the forecast class

--- VremenskaPrognoza/MainWindow.xaml.cs
+++ VremenskaPrognoza/MainWindow.xaml.cs
@@ -185,7 +185,9 @@ namespace VremenskaPrognoza
             public long date_epoch { get; set; }
             public Day day { get; set; }
             public Astro astro { get; set; }
+            public List<Hour> hour { get; set; }
         }
+        
         public class Day
         {
             public double maxtemp_c { get; set; }
@@ -207,6 +209,43 @@ namespace VremenskaPrognoza
             public int daily_will_it_snow { get; set; }
             public int daily_chance_of_snow { get; set; }
             public Condition condition { get; set; }
+            public double uv { get; set; }
+            
+        }
+        public class Hour
+        {
+            public int time_epoch { get; set; }
+            public string time { get; set; }
+            public double temp_c { get; set; }
+            public double temp_f { get; set; }
+            public int is_day { get; set; }
+            public Condition condition { get; set; }
+            public double wind_mph { get; set; }
+            public double wind_kph { get; set; }
+            public int wind_degree { get; set; }
+            public string wind_dir { get; set; }
+            public double pressure_mb { get; set; }
+            public double pressure_in { get; set; }
+            public double precip_mm { get; set; }
+            public double precip_in { get; set; }
+            public int humidity { get; set; }
+            public int cloud { get; set; }
+            public double feelslike_c { get; set; }
+            public double feelslike_f { get; set; }
+            public double windchill_c { get; set; }
+            public double windchill_f { get; set; }
+            public double heatindex_c { get; set; }
+            public double heatindex_f { get; set; }
+            public double dewpoint_c { get; set; }
+            public double dewpoint_f { get; set; }
+            public int will_it_rain { get; set; }
+            public int chance_of_rain { get; set; }
+            public int will_it_snow { get; set; }
+            public int chance_of_snow { get; set; }
+            public double vis_km { get; set; }
+            public double vis_miles { get; set; }
+            public double gust_mph { get; set; }
+            public double gust_kph { get; set; }
             public double uv { get; set; }
         }
         public class Astro
